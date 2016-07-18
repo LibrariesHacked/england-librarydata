df92d1f16632ed4efb3976ae296750ae5b40776c
Dave <david_rowe@bathnes.gov.uk>
2016-07-18 22:48:53 +0100

    Getting started on map data.

---
+++ js/map.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    /////////////////////////////////////////////////
+    // Map
+    // Initialise the map, set center, zoom, etc.
+    /////////////////////////////////////////////////
+    var map = L.map('map', { zoomControl: false }).setView([51.15, 2.72], 13);
+    L.tileLayer('http://{s}.tiles.mapbox.com/v3/librarieshacked.jefmk67b/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    //add zoom control with your options
+    L.control.zoom({
+        position: 'topright'
+    }).addTo(map);
+});
