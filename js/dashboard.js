0ff474ff80287ce93333d66f016a15724068d4ea
Dave <david_rowe@bathnes.gov.uk>
2016-07-26 22:47:44 +0100

    Adding graphing library

--- js/dashboard.js
+++ js/dashboard.js
@@ -73,5 +73,28 @@
             marker.addTo(map);
             newsMarkers.push(marker);
         });
+
+
+        //2.
+
+
+
+
+
+
+        // 3. 
+        new Morris.Line({
+            element: 'divStoriesLineChart',
+            data: [
+              { year: '2008', value: 20 },
+              { year: '2009', value: 10 },
+              { year: '2010', value: 5 },
+              { year: '2011', value: 5 },
+              { year: '2012', value: 20 }
+            ],
+            xkey: 'year',
+            ykeys: ['value'],
+            labels: ['Value']
+        });
     });
 });
