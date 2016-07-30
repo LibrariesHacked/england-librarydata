7ee7b92c00de80b8b864d547743408ea79457975
Dave <david_rowe@bathnes.gov.uk>
2016-07-30 11:36:12 +0100

    Working on mini news map

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,8 +3,6 @@
     // Map
     // Initialise the map, set center, zoom, etc.
     /////////////////////////////////////////////////
-    var changesMarkers = [];
-    var newsMarkers = [];
     var map = L.map('divMiniMap', { zoomControl: false }).setView([52.55, -2.72], 7);
     L.tileLayer('http://{s}.tiles.mapbox.com/v3/librarieshacked.jefmk67b/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -15,22 +13,21 @@
 
     // Load the initial set of data - for the dashboard start with 1 month
     PublicLibrariesNews.loadData(2, function () {
-        var local = PublicLibrariesNews.getStoriesGroupedByLocation('local');
-        var changes = PublicLibrariesNews.getStoriesGroupedByLocation('changes');
+        var data = { 'local': PublicLibrariesNews.getStoriesGroupedByLocation('local'), 'changes': PublicLibrariesNews.getStoriesGroupedByLocation('changes') }
 
         //////////////////////////////////////////////
         // 1. Populate the changes
         //////////////////////////////////////////////
         var currentlyShowing = [0, 2];
-        var locs = Object.keys(changes);
+        var locs = Object.keys(data['changes']);
         locs.sort(function (a, b) {
-            return changes[b].stories.length - changes[a].stories.length;
+            return data['changes'][b].stories.length - data['changes'][a].stories.length;
         });
 
         var clickChangeItem = function (event) {
             event.preventDefault();
             var item = $(event.currentTarget);
-            var authSt = changes[$(item).data('auth')].stories;
+            var authSt = data['changes'][$(item).data('auth')].stories;
             var index = $(item).data('current') + 1;
             if (index == authSt.length) index = 0;
             $(item).data('current', index);
@@ -39,7 +36,7 @@
         };
 
         var addLocation = function (index, position) {
-            var it = changes[locs[index]];
+            var it = data['changes'][locs[index]];
             var li = '<a href="#" id="aChangeLocation' + index + '" class="list-group-item changes-list" data-current="0" data-auth="' + locs[index] + '"><span class="badge">1/' + it.stories.length + '</span><h4 class="list-group-item-heading">' + locs[index] + '</h5><p class="list-group-item-text">' + it.stories[0].text + '</p></a>';
             position == 'first' ? $('#divChangesCounts').prepend(li) : $('#divChangesCounts').append(li);
             $('#aChangeLocation' + index).on('click', clickChangeItem);
@@ -69,31 +66,35 @@
         //////////////////////////////////////////////
         // 2. Populate the mini map
         //////////////////////////////////////////////
-        var miniMapCurrent = 0;
-        $.each(local, function (i, o) {
-            var size = ['small', 20];
-            if (o.stories.length >= 5) size = ['medium', 30];
-            if (o.stories.length >= 10) size = ['large', 40];
-            var newsIcon = L.divIcon({ html: '<div><span>' + o.stories.length + '</span></div>', className: "marker-cluster marker-cluster-" + size[0], iconSize: new L.Point(size[1], size[1]) });
-            var marker = L.marker([o.lat, o.lng], { icon: newsIcon });
-            marker.stories = o.stories;
-            marker.title = i;
-            // Attach a click event to the marker.
-            var markerClick = function (e) {
-                // Set the modal content
-                $('#divMiniMapText').empty();
-                $('#divMiniMapText').show(500);
-                $.each(e.target.stories, function () {
-                    var summary = this.text.substring(0, 100) + '&#8230;';
-                    $('#divMiniMapText').append(summary);
+        var miniMapCurrent = 'local';
+        var mapLayers = { 'local': '', 'changes': '' };
+        var addMiniMapPoints = function (type) {
+            if (mapLayers[type] == '') {
+                var markerArray = [];
+                $.each(data[type], function (i, o) {
+                    var size = ['small', 20];
+                    if (o.stories.length >= 5) size = ['medium', 30];
+                    if (o.stories.length >= 10) size = ['large', 40];
+                    var newsIcon = L.divIcon({ html: '<div><span>' + o.stories.length + '</span></div>', className: "marker-cluster marker-cluster-" + size[0], iconSize: new L.Point(size[1], size[1]) });
+                    var marker = L.marker([o.lat, o.lng], { icon: newsIcon });
+                    marker.stories = o.stories;
+                    marker.title = i;
+                    var markerClick = function (e) {
+                    };
+                    marker.on('click', markerClick);
+                    markerArray.push(marker);
                 });
-            };
-            marker.on('click', markerClick);
-            marker.addTo(map);
-            newsMarkers.push(marker);
+                mapLayers[type] = L.layerGroup(markerArray);
+            }
+            mapLayers[type].addTo(map);
+        };
+        $('#ulMapPointType li span').first().text(Object.keys(data['local']).length);
+        $('#ulMapPointType li span').last().text(Object.keys(data['changes']).length);
+        $('#ulMapPointType li span a').first().click(function (e) {
+            e.preventDefault();
+            if (miniMapCurrent == 'local') return false;
         });
-        $('#ulMapPointType li span').first().text(Object.keys(local).length);
-        $('#ulMapPointType li span').last().text(Object.keys(changes).length);
+        addMiniMapPoints('changes');
 
         // 3. The stories line chart
         new Morris.Line({
