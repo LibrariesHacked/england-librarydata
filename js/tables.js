9d17a3a489fc0e5309bf6fe97fc2686e45a84e01
Dave <dave_roweuk@yahoo.co.uk>
2016-09-19 18:59:41 +0100

    table components

---
+++ js/tables.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+
+
+    /////////////////////////////////////////////////
+    // Table 1: Authorities
+    /////////////////////////////////////////////////
+    var tableAuthorities = $('#tblAuthorities').dataTable(
+        {
+            processing: true,
+            responsive: true,
+            dom: 'Bfrtip',
+            buttons: [
+                { extend: 'print', text: 'Print', className: 'btn-sm' },
+                { extend: 'excelHtml5', text: 'Export Excel', className: 'btn-sm' }
+            ],
+            deferRender: true,
+            data: [],//PublicLibrariesNews.getAuthoritiesDataTable(),
+            columns: [
+                { title: "Authority name" },
+                { title: "Code", visible: false },
+                { title: "Number libraries" },
+                { title: "Local authority libraries" },
+                { title: "Commissioned libraries" },
+                { title: "Community libraries" },
+                { title: "Independent community libraries" },
+                { title: "Closed libraries" },
+                { title: "Population" },
+                { title: "Area (hecatares)" }
+            ],
+            order: [[8, 'asc']]
+        });
+});
