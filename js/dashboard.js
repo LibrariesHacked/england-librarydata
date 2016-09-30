50fef4ea2bf980cdde31d85bbc759368068b0934
Dave <dave_roweuk@yahoo.co.uk>
2016-09-30 16:26:33 +0100

    Format error

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,7 +7,7 @@
         if (num >= 1000000000) return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
         if (num >= 1000000) return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
         if (num >= 1000) return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
-        return num;
+        return num.toFixed(0);
     };
 
     /////////////////////////////////////////////////
