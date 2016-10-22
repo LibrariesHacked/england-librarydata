d87f489bf0359b32b4fd2e5fe3bfdec06d6de405
Dave <dave_roweuk@yahoo.co.uk>
2016-10-22 12:00:53 +0100

    Minor design changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -333,31 +333,23 @@
                 labels: [0, 1, 2, 3, 4, 5, 6],
                 datasets: [
                     {
-                        //label: "My First dataset",
-                        //fill: false,
-                        //lineTension: 0.1,
-                        //backgroundColor: "rgba(75,192,192,0.4)",
-                        //borderColor: "rgba(75,192,192,1)",
-                        //borderCapStyle: 'butt',
-                        //borderDash: [],
-                        //borderDashOffset: 0.0,
-                        //borderJoinStyle: 'miter',
-                        //pointBorderColor: "rgba(75,192,192,1)",
-                        //pointBackgroundColor: "#fff",
-                        //pointBorderWidth: 1,
-                        //pointHoverRadius: 5,
-                        //pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                        //pointHoverBorderColor: "rgba(220,220,220,1)",
-                        //pointHoverBorderWidth: 2,
-                        //pointRadius: 1,
-                        //pointHitRadius: 10,
                         data: [65, 59, 80, 81, 56, 55, 40],
                         spanGaps: false
                     }
                 ]
             },
             options: {
-                animation: { animateScale: true }
+                animation: { animateScale: true },
+                legend: {
+                    display: false
+                },
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            maxTicksLimit: 8
+                        }
+                    }]
+                }
             }
         });
         var updateLibDistancesLine = function (authority) {
@@ -365,9 +357,17 @@
             distanceLine.config.data.labels = $.map(distances, function (x, y) { if (y != 'undefined') return y; });
             distanceLine.config.data.datasets[0].data = $.map(distances, function (x, y) { return x; });
             distanceLine.update();
+            var totalDistance = 0, population = 0;
+            $.each(distances, function (i, x) {
+                totalDistance = (totalDistance + (i * x));
+                population = population + x
+            })
+            $('#divDistanceAverage p').text((totalDistance / population).toFixed(1) + ' miles');
         };
         updateLibDistancesLine();
         $.each(PublicLibrariesNews.getAuthorityListSorted(), function (i, x) { $('#selAuthority').append($("<option></option>").attr("value", x).text(x)); });
+
+
         // EVENT: Change authority
         $('#selAuthority').change(function () {
             var auth = $('#selAuthority').find(":selected").val();
